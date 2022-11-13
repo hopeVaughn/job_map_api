197366e947edcf4cf686435ce76598975df3796c
hopeVaughn <hopevaughnwarren@gmail.com>
2022-11-13 09:20:14 -0800

    add modules and env variables

---
+++ database.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+const { query } = require("express");
+const { Pool } = require("pg");
+const dbParams = require("./lib/db.js");
+const db = new Pool(dbParams);
+db.connect();
