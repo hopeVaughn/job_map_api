d1c80b253f488b1ee678bc2e7a5a28e0b883c60e
hopeVaughn <hopevaughnwarren@gmail.com>
2022-11-13 09:32:24 -0800

    add reset db function and routes

---
+++ server.js
@@ -0,0 +1,31 @@
+// load .env data into process.env // load the .env!
+require("dotenv").config();
+// Web server config
+const PORT = process.env.PORT || 8080;
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require('body-parser');
+// PG database client/connection setup
+const { Pool } = require("pg");
+const dbParams = require("./lib/db.js");
+const db = new Pool(dbParams);
+db.connect();
+
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
