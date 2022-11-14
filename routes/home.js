2c5d389da454e8fcc3ac57835abce7e7582df75f
hopeVaughn <hopevaughnwarren@gmail.com>
2022-11-14 11:17:52 -0800

    change routes to async await

--- routes/home.js
+++ routes/home.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
+// module.exports = (db) => {
+//   router.get('/', (req, res) => {
+//     const resumeSentQuery = `SELECT * FROM companies WHERE resume_sent = TRUE;`
+//     db.query(resumeSentQuery)
+//       .then((result) => {
+//         res.json(result.rows.length)
+//         return result.rows.length;
+//       })
+//       .catch((err) => {
+//         console.error(err.message)
+//       })
+//   })
+//   return router;
+// }
+
 module.exports = (db) => {
-  router.get('/', (req, res) => {
+  router.get('/', async (req, res) => {
     const resumeSentQuery = `SELECT * FROM companies WHERE resume_sent = TRUE;`
-    db.query(resumeSentQuery)
-      .then((result) => {
-        res.json(result.rows.length)
-        return result.rows.length;
-      })
-      .catch((err) => {
-        console.error(err.message)
-      })
+    try {
+      const result = await db.query(resumeSentQuery);
+      res.json(result.rows.length)
+      return result.rows.length
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server error")
+    }
   })
   return router;
 }
