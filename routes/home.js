8cb3a5fb6dc869bdb54ddfe81dc1a984d7a95b00
hopeVaughn <hopevaughnwarren@gmail.com>
2022-11-14 12:42:27 -0800

    added test directory

--- routes/home.js
+++ routes/home.js
@@ -19,14 +19,23 @@ const router = express.Router();
 module.exports = (db) => {
   router.get('/', async (req, res) => {
     const resumeSentQuery = `SELECT * FROM companies WHERE resume_sent = TRUE;`
+    const hrInterviewQuery = `SELECT * FROM companies WHERE hr_interview = TRUE;`
+    const techInterviewQuery = `SELECT * FROM companies WHERE tech_interview = TRUE;`
+    const offerQuery = `SELECT * FROM companies WHERE job_offer = TRUE;`
     try {
-      const result = await db.query(resumeSentQuery);
-      res.json(result.rows.length)
-      return result.rows.length
+      const resume = await db.query(resumeSentQuery);
+      const hrInterview = await db.query(hrInterviewQuery);
+      const techInterview = await db.query(techInterviewQuery);
+      const jobOffer = await db.query(offerQuery);
+      // res.json(resume.rows.length)
+      // res.json(hrInterview.rows.length)
+      // res.json(techInterview.rows.length)
+      res.json(jobOffer.rows.length)
+      return resume.rows.length, hrInterview.rows.length, techInterview.rows.length, jobOffer.rows.length
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Server error")
     }
-  })
+  });
   return router;
 }
