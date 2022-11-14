c3227c297d1659b4cb13562ed273b58d4bf921b6
hopeVaughn <hopevaughnwarren@gmail.com>
2022-11-14 14:56:09 -0800

    add networks and contacts db tables

---
+++ db/schema/04_contact.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS contact CASCADE;
+CREATE TABLE contacts(
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(250) NOT NULL,
+  network_img VARCHAR(250),
+  linkedin VARCHAR(250),
+  twitter VARCHAR(250),
+  github VARCHAR(250),
+)
