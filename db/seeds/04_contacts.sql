6a8ad09719a3ac3ec1af17e55c99daa6d5233968
Eduardo <cesconettoedu@gmail.com>
2022-11-24 11:36:14 -0500

    Pleeeeaaaassseeee add this Seeds

---
+++ db/seeds/04_contacts.sql
@@ -0,0 +1,20 @@
+INSERT INTO contacts
+(user_id, name, image, linkedin, github, twitter)
+ VALUES
+ ('5c2ea821-8462-4c2b-8bb7-eb1b30739837', 'Eduardo',
+ 'https://media-exp1.licdn.com/dms/image/C5603AQEeD8y12M9gEA/profile-displayphoto-shrink_200_200/0/1658772472251?e=1671062400&v=beta&t=liwzekkO3CPQLIcsULENoUA_3lqBNRFg9lFrwVE_EPg',
+ 'https://www.linkedin.com/in/eduardo-cesconetto/',
+ 'https://github.com/cesconettoedu',
+ 'https://twitter.com/'),
+
+ ('5c2ea821-8462-4c2b-8bb7-eb1b30739837', 'Hope',
+ 'https://media-exp1.licdn.com/dms/image/D5635AQEZel_5G8Vb6w/profile-framedphoto-shrink_800_800/0/1660263415438?e=1669910400&v=beta&t=HpFOMFTFwRJB2LfpLKYSACyeMb_vW1JWOSwcvLwB3bY',
+ 'https://www.linkedin.com/in/hope-warren-84488423a/',
+ 'https://github.com/hopeVaughn',
+ 'https://twitter.com/'),
+
+ ('5c2ea821-8462-4c2b-8bb7-eb1b30739837', 'Nadia',
+ 'https://media-exp1.licdn.com/dms/image/C4E03AQGU2tgiWgijXA/profile-displayphoto-shrink_200_200/0/1636528641142?e=1674691200&v=beta&t=P-n5w5QxJJ9B6Rp0KqvAR1O-gCEpx5fhNAdt39381Sc',
+ 'https://www.linkedin.com/in/nadia-piatetskaia-76a34a216/',
+ 'https://github.com/NadiaPia',
+ 'https://twitter.com/');
