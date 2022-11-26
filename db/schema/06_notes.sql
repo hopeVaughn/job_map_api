bb120649ea9de9f7c7029f3304c45d8a85ebcd0f
hopeVaughn <hopevaughnwarren@gmail.com>
2022-11-26 12:40:18 -0800

    change to notes table

--- db/schema/06_notes.sql
+++ db/schema/06_notes.sql
@@ -2,5 +2,6 @@ DROP TABLE IF EXISTS notes CASCADE;
 CREATE TABLE notes(
   id SERIAL PRIMARY KEY NOT NULL,
   application_id INT NOT NULL,
-  note VARCHAR(250)
+  note VARCHAR(250),
+  timestamp INT
 );
