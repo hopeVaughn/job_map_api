7c87f9d463a5aa8f3fd95c81a23d7859360010c3
Eduardo <cesconettoedu@gmail.com>
2022-11-29 00:53:38 -0500

    network is add by companyid of aplication, note hardcode anymore

--- db/seeds/04_contacts.sql
+++ db/seeds/04_contacts.sql
@@ -1,17 +1,43 @@
 INSERT INTO contacts
 (user_id, name, image, linkedin, github, twitter)
  VALUES
- ('5c2ea821-8462-4c2b-8bb7-eb1b30739837', 'Eduardo',
+ ('5c2ea821-8462-4c2b-8bb7-eb1b30739837', 'Hope Warren',
+ 'https://media-exp1.licdn.com/dms/image/D5635AQEZel_5G8Vb6w/profile-framedphoto-shrink_800_800/0/1660263415438?e=1669910400&v=beta&t=HpFOMFTFwRJB2LfpLKYSACyeMb_vW1JWOSwcvLwB3bY',
+ 'https://www.linkedin.com/in/hope-warren-84488423a/',
+ 'https://github.com/hopeVaughn',
+ 'https://twitter.com/hopeVaughn80'),
+
+ ('5c2ea821-8462-4c2b-8bb7-eb1b30739837', 'Eduardo Cesconetto',
  'https://media-exp1.licdn.com/dms/image/C5603AQEeD8y12M9gEA/profile-displayphoto-shrink_200_200/0/1658772472251?e=1671062400&v=beta&t=liwzekkO3CPQLIcsULENoUA_3lqBNRFg9lFrwVE_EPg',
  'https://www.linkedin.com/in/eduardo-cesconetto/',
  'https://github.com/cesconettoedu',
  'https://twitter.com/'),
 
- ('5c2ea821-8462-4c2b-8bb7-eb1b30739837', 'Hope',
- 'https://media-exp1.licdn.com/dms/image/D5635AQEZel_5G8Vb6w/profile-framedphoto-shrink_800_800/0/1660263415438?e=1669910400&v=beta&t=HpFOMFTFwRJB2LfpLKYSACyeMb_vW1JWOSwcvLwB3bY',
- 'https://www.linkedin.com/in/hope-warren-84488423a/',
- 'https://github.com/hopeVaughn',
- 'https://twitter.com/'),
+  ('5c2ea821-8462-4c2b-8bb7-eb1b30739837', 'John Smilga',
+ 'https://media-exp1.licdn.com/dms/image/C5603AQHjVrQbpO0QJg/profile-displayphoto-shrink_100_100/0/1590845425839?e=1674691200&v=beta&t=4FAzgR-2aUjZ7qlzqa2fQwcCVfHZj3QU2oQjy8UQRyA',
+ 'https://www.linkedin.com/in/john-smilga-659100177/',
+ 'https://github.com/john-smilga',
+ 'https://twitter.com/john_smilga'),
+
+
+ ('5c2ea821-8462-4c2b-8bb7-eb1b30739837', 'Lex Fridman',
+ 'https://media-exp1.licdn.com/dms/image/C4E03AQHLCrHhGZ0xMg/profile-displayphoto-shrink_400_400/0/1519486750427?e=1674691200&v=beta&t=doUFfQsXHOqJtueL_VXdVsqHB0VU_KMg0UFlabb4Z3E',
+ 'https://www.linkedin.com/in/lexfridman/',
+ 'https://github.com/',
+ 'https://twitter.com/lexfridman'),
+
+
+ ('5c2ea821-8462-4c2b-8bb7-eb1b30739837', 'Wes Bos',
+ 'https://media-exp1.licdn.com/dms/image/D5603AQFAeDDva4zxiQ/profile-displayphoto-shrink_400_400/0/1668185805196?e=1674691200&v=beta&t=-PmgmR0dqjhqq1kMoF-ohqs58Rcl1lVS7u6PcbtD0Vc',
+ 'https://www.linkedin.com/in/wesbos/',
+ 'https://github.com/wesbos',
+ 'https://twitter.com/wesbos'),
+
+ ('5c2ea821-8462-4c2b-8bb7-eb1b30739837', 'Ania Kubów',
+ 'https://media-exp1.licdn.com/dms/image/C5603AQE9vb2BZBhkdw/profile-displayphoto-shrink_400_400/0/1618581460461?e=1674691200&v=beta&t=_GQjstjo9FmeoohBOTz8DyG5D8rS7hvsS4JhYJZChDA',
+ 'https://www.linkedin.com/in/ania-kubow/',
+ 'https://github.com/kubowania',
+ 'https://twitter.com/ania_kubow'),
 
  ('5c2ea821-8462-4c2b-8bb7-eb1b30739837', 'Nadia',
  'https://media-exp1.licdn.com/dms/image/C4E03AQGU2tgiWgijXA/profile-displayphoto-shrink_200_200/0/1636528641142?e=1674691200&v=beta&t=P-n5w5QxJJ9B6Rp0KqvAR1O-gCEpx5fhNAdt39381Sc',
