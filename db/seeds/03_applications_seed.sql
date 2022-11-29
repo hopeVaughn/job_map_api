d8e6df742d019ddbb68ed41d4cd8a5e14acfcab7
hopeVaughn <hopevaughnwarren@gmail.com>
2022-11-29 10:08:32 -0800

    add more changes to application seeds

--- db/seeds/03_applications_seed.sql
+++ db/seeds/03_applications_seed.sql
@@ -1,6 +1,12 @@
 INSERT INTO applications(resume_sent,hr_interview,tech_interview,job_offer,company_id) VALUES (TRUE, FALSE, FALSE, FALSE,4);
+INSERT INTO applications(resume_sent,hr_interview,tech_interview,job_offer,company_id) VALUES (TRUE, FALSE, FALSE, FALSE,3);
+INSERT INTO applications(resume_sent,hr_interview,tech_interview,job_offer,company_id) VALUES (TRUE, FALSE, FALSE, FALSE,2);
+INSERT INTO applications(resume_sent,hr_interview,tech_interview,job_offer,company_id) VALUES (TRUE, FALSE, FALSE, FALSE,1);
 INSERT INTO applications(resume_sent,hr_interview,tech_interview,job_offer,company_id) VALUES (FALSE, TRUE, FALSE, FALSE,3);
+INSERT INTO applications(resume_sent,hr_interview,tech_interview,job_offer,company_id) VALUES (FALSE, TRUE, FALSE, FALSE,2);
+INSERT INTO applications(resume_sent,hr_interview,tech_interview,job_offer,company_id) VALUES (FALSE, TRUE, FALSE, FALSE,1);
 INSERT INTO applications(resume_sent,hr_interview,tech_interview,job_offer,company_id) VALUES (FALSE, FALSE, TRUE, FALSE,1);
+INSERT INTO applications(resume_sent,hr_interview,tech_interview,job_offer,company_id) VALUES (FALSE, FALSE, TRUE, FALSE,2);
 INSERT INTO applications(resume_sent,hr_interview,tech_interview,job_offer,company_id) VALUES (FALSE, FALSE, FALSE, TRUE,5);
 INSERT INTO applications(resume_sent,hr_interview,tech_interview,job_offer,company_id) VALUES (TRUE, FALSE, FALSE, FALSE,2);
 
